7ffab4431de85caecfe8344421e1d297fa993aeb
Preston Phelan <preston.phelan@gmail.com>
2017-01-10 14:50:02 -0800

    Store done

--- frontend/ananta.jsx
+++ frontend/ananta.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
+
+//TODO Remove after testing
+  window.store = configureStore();
+//END
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
   ReactDOM.render(<div>It's working!!!</div>, root);
 });
