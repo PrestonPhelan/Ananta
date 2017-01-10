09fcfd63023deefd0b62be96b4e365a46b16a3a3
Preston Phelan <preston.phelan@gmail.com>
2017-01-10 14:43:49 -0800

    SessionReducer and RootReducer done

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,26 @@
+import merge from 'lodash/merge';
+
+import {
+  RECEIVE_CURRENT_USER,
+  RECEIVE_ERRORS
+} from '../actions/session_actions';
+
+const _nullUser = {
+  currentUser: "",
+  errors: []
+};
+
+export default (state = _nullUser, action) => {
+  Object.freeze(state);
+  let newState = merge({}, state);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      newState.currentUser = action.currentUser;
+      return newState;
+    case RECEIVE_ERRORS:
+      newState.errors = action.errors;
+      return newState;
+    default:
+      return state;
+  }
+};
