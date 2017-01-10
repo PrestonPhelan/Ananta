b55971f07f99049c3341f4f4643ec7484940ee03
Preston Phelan <preston.phelan@gmail.com>
2017-01-10 14:56:24 -0800

    App and root components updated, rendered through entry file

--- frontend/ananta.jsx
+++ frontend/ananta.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import configureStore from './store/store';
-
-//TODO Remove after testing
-  window.store = configureStore();
-//END
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
-  ReactDOM.render(<div>It's working!!!</div>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
---
+++ frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = ({ children }) => (
+  <div>
+    <h1>Ananta</h1>
+    <h3>It's working!!!</h3>
+    { children }
+  </div>
+);
+
+export default App;
