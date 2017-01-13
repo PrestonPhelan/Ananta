c7748ed31cede7860af98c5861d3558674974edd
Preston Phelan <preston.phelan@gmail.com>
2017-01-13 13:12:31 -0800

    Fixed seed data to create organization

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,15 @@
 User.delete_all
 Team.delete_all
 Membership.delete_all
+Organization.delete_all
 
 users = User.create([
   {username: 'GuybrushThreepwood', email: 'guybrush@ananta.com', password: 'password', image_url: 'http://rs405.pbsrc.com/albums/pp131/karkela/guybrush.jpg~c200'},
   {username: 'Guest', email: 'guest@ananta.com', password: 'password', image_url: 'http://rs405.pbsrc.com/albums/pp131/karkela/guybrush.jpg~c200'}])
+
+organizations = Organization.create([
+  {name: 'Monkey Island', owner_id: 1}
+  ])
 
 teams = Team.create([
   {name: 'Crew To Monkey Island', owner_id: 1},
