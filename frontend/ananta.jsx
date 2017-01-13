5b6000aeb187502b19b384f0ac96ae515326148b
Preston Phelan <preston.phelan@gmail.com>
2017-01-13 14:31:32 -0800

    Users action creator & reducer

--- frontend/ananta.jsx
+++ frontend/ananta.jsx
@@ -5,8 +5,7 @@ import configureStore from './store/store';
 import Root from './components/root';
 
 //TODO Remove after testing
-// import { fetchUsers } from './actions/user_actions';
-import { fetchUsers } from './util/user_api_util';
+import { fetchUsers } from './actions/user_actions';
 //END
 
 document.addEventListener('DOMContentLoaded', () => {
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import SessionReducer from './session_reducer';
 import TeamsReducer from './teams_reducer';
+import UsersReducer from './users_reducer';
 
 export default combineReducers({
   session: SessionReducer,
-  teams: TeamsReducer
+  teams: TeamsReducer,
+  users: UsersReducer
 });
