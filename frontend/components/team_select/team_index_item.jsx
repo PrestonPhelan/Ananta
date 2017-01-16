1a24ef2a20fd537b60be9fcddbd8fbd58da4d76b
Preston Phelan <preston.phelan@gmail.com>
2017-01-15 20:35:16 -0800

    Team index items now link to team view pages

--- frontend/components/team_select/team_index_item.jsx
+++ frontend/components/team_select/team_index_item.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { hashHistory } from 'react-router';
 
 import MemberListContainer from './member_list_container';
 
 export default props => (
-  <div className='team-index-item'>
+  <div className='team-index-item' onClick={() => hashHistory.push(`/app/team/${props.team.id}`)}>
     {props.team.name}
     <MemberListContainer members={props.team.members} />
   </div>
