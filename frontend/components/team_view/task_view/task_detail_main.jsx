8fdb7b172b548b05687e2589233def934ef94d48
Preston Phelan <preston.phelan@gmail.com>
2017-01-18 18:07:07 -0800

    Everything renders in task detail

--- frontend/components/team_view/task_view/task_detail_main.jsx
+++ frontend/components/team_view/task_view/task_detail_main.jsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => (
-  <div></div>
-);
+class TaskDetailMain extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.task.completed ?
+          <i id='completed-check' className="fa fa-check-circle" aria-hidden="true" onClick={this.updateCompleted}></i> :
+          <i id='incomplete-check' className="fa fa-check-circle-o" aria-hidden="true" onClick={this.updateCompleted}></i>}
+        {this.props.task.name}
+      </div>
+    );
+  }
+}
+
+export default TaskDetailMain;
